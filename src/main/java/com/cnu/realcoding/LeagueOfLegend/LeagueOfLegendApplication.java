ac054fc2369984b6248cf32cf86b84e02460682f
leehyunjun <zxzxs9182@gmail.com>
2019-04-15 22:52:52 +0900

    make project Use spring boot intialize

---
+++ src/main/java/com/cnu/realcoding/LeagueOfLegend/LeagueOfLegendApplication.java
@@ -0,0 +1,13 @@
+package com.cnu.realcoding.LeagueOfLegend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueOfLegendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueOfLegendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cnu/realcoding/LeagueOfLegend/LeagueOfLegendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cnu.realcoding.LeagueOfLegend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LeagueOfLegendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
