2c1973490372d1a39423afd2a4056e27bd60e6b9
leehyunjun <zxzxs9182@gmail.com>
2019-04-15 23:30:21 +0900

    make domain package and response body

--- src/main/java/com/cnu/realcoding/LeagueOfLegend/LeagueOfLegendApplication.java
+++ src/main/java/com/cnu/realcoding/LeagueOfLegend/LeagueOfLegendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LeagueOfLegendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LeagueOfLegendApplication.class, args);
 	}
 
---
+++ src/main/java/com/cnu/realcoding/LeagueOfLegend/domain/requestSummonerID.java
@@ -0,0 +1,14 @@
+package com.cnu.realcoding.LeagueOfLegend.domain;
+
+import lombok.Data;
+
+@Data
+public class requestSummonerID {
+    private int profileIconId;
+    private String name;
+    private String puuid;
+    private int summonerLevel;
+    private String accountID;
+    private String Id;
+    private int revisionDate;
+}
