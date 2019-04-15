12b9c5b8e6368c01bb55b357525c698eca90f240
leehyunjun <zxzxs9182@gmail.com>
2019-04-16 00:08:04 +0900

    Define a service in controller

---
+++ src/main/java/com/cnu/realcoding/LeagueOfLegend/controller/summonerIDController.java
@@ -0,0 +1,26 @@
+package com.cnu.realcoding.LeagueOfLegend.controller;
+
+import com.cnu.realcoding.LeagueOfLegend.domain.requestSummonerID;
+import com.cnu.realcoding.LeagueOfLegend.service.summonerIDService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class summonerIDController {
+
+    @Autowired
+    private summonerIDService summonerIDService;
+
+    @PostMapping("/summonerid")
+    public void insertDog(@RequestBody requestSummonerID summonerID){
+        summonerIDService.insertSummonerID(summonerID);
+    }
+
+    @GetMapping("/summonerid")
+   public List<requestSummonerID> getAllSummonerID(){
+    return summonerIDService.getAllSUmmoner();
+    }
+}
