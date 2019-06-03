dc02513da92df36945f02fde4aa02bc8d371c366
leehyunjun <zxzxs9182@gmail.com>
2019-06-03 14:51:48 +0900

    Change Parameter value

--- src/main/java/com/cnu/realcoding/LeagueOfLegend/repository/summonerRepository.java
+++ src/main/java/com/cnu/realcoding/LeagueOfLegend/repository/summonerRepository.java
@@ -1,6 +1,7 @@
 package com.cnu.realcoding.LeagueOfLegend.repository;
 
 
+import com.cnu.realcoding.LeagueOfLegend.domain.requestSummonerBody;
 import com.cnu.realcoding.LeagueOfLegend.domain.requestSummonerID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -12,9 +13,9 @@ public class summonerRepository {
     @Autowired
     private MongoTemplate mongoTemplate;
 
-    public void insertsummonerIDBody(requestSummonerID requestSummonerID){
+    public void insertsummonerIDBody(requestSummonerBody requestSummonerBOdy){
 
-        mongoTemplate.insert(requestSummonerID);
+        mongoTemplate.insert(requestSummonerBOdy);
 
     }
 }
